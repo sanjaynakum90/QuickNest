2ad06690979e37f83cc87fab158ee78f064b0015
sanjaynakum90 <sanjaynakum4545@gmail.com>
2026-03-25 10:32:07 +0530

    project started

---
+++ server.js
@@ -0,0 +1,46 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config({ path: "./.env" })
+
+import HttpError from "./middleware/HttpError.js"
+import connectDB from "./config/db.js"
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.status(200).json("hello form server")
+})
+
+app.use((req, res, next) => {
+    next(new HttpError("requested routes not found", 404))
+})
+
+app.use((error, req, res, next) => {
+    if (req.headerSent) {
+        next(error)
+    }
+
+    res.status(error.statusCode || 500).json({ message: error.message || "internal server error" })
+})
+
+const port = process.env.PORT || 5000
+
+async function startServer() {
+    try {
+        await connectDB()
+
+        app.listen(port, () => {
+            console.log(`server running on port ${port}`);
+
+        })
+    } catch (error) {
+
+        console.log(error.message);
+
+        process.exit(1)
+    }
+}
+
+startServer()
